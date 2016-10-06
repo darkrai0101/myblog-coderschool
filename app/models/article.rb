0b388aac0dedb9e1f64af7019dea5132f0d11e81
darkrai0101 <nv.trung91@gmail.com>
2016-10-06 14:18:40 +0700

    fix some coding style

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   has_many:comments, dependent: :destroy 
-	def self.search(search)
-	  where("title ILIKE ? OR body ILIKE ?", "%#{search}%", "%#{search}%") 
-	  # where("body LIKE ?", "%#{search}%")
-	end
+  def self.search(search)
+    where("title ILIKE ? OR body ILIKE ?", "%#{search}%", "%#{search}%") 
+	# where("body LIKE ?", "%#{search}%")
+  end
 end
