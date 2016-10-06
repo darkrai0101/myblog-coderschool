c9ee81451847ecea38b3815ae5545d2cff697949
darkrai0101 <nv.trung91@gmail.com>
2016-10-06 14:03:59 +0700

    fixing bug issues 1

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
-	has_many:comments, dependent: :destroy 
+  has_many:comments, dependent: :destroy 
 	def self.search(search)
-	  where("title LIKE ?", "%#{search}%") 
+	  where("title ILIKE ? OR body ILIKE ?", "%#{search}%", "%#{search}%") 
 	  # where("body LIKE ?", "%#{search}%")
 	end
 end
